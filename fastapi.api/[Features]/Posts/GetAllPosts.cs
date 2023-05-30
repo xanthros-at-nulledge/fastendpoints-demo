794dde87cb5f16b78b6fe3339723157a68e491d4
Ranil Arieta <ranil.arieta@nulledge.net>
2023-05-30 00:24:45 -0700

    initial commit

---
+++ fastapi.api/[Features]/Posts/GetAllPosts.cs
@@ -0,0 +1,28 @@
+﻿using System.Dynamic;
+using System.Text.Json.Nodes;
+using fastapi.api._Features_.Posts.Models;
+
+namespace fastapi.api._Features_.Posts;
+
+public class GetAllPosts : EndpointWithoutRequest
+{
+    private HttpClient _httpClient;
+    public GetAllPosts(IHttpClientFactory clientFactory)
+    {
+        this._httpClient = clientFactory.CreateClient();
+    }
+
+    public override void Configure()
+    {
+        Get("/posts");
+        AllowAnonymous();
+    }
+    
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var request = await this._httpClient.GetFromJsonAsync<IList<Post>>("https://jsonplaceholder.typicode.com/posts");
+
+        //dynamic body = await request.Content.ReadFromJsonAsync<ExpandoObject>();
+        await SendAsync(request);
+    }
+}
---
+++ fastapi.api/[Features]/Posts/Models/Post.cs
@@ -0,0 +1,9 @@
+﻿namespace fastapi.api._Features_.Posts.Models;
+
+public class Post
+{
+    public int id { get; set; }
+    public string title { get; set; }
+    public string body { get; set; }
+    public int userId { get; set; }
+}
